6e97846d5ae274a578b3a6fda85223e81a3c53b8
dipesh <jaswanidipesh8@gmail.com>
2023-03-21 06:04:13 +0530

    backend-added

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+const mongoose = require('mongoose');
+const cors = require('cors')
+
+
+const authRoutes = require('./routes/auth')
+const testRoutes = require('./routes/report')
+
+const app = express();
+
+app.use(cors())
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+//routes 
+
+app.use('/api',authRoutes)
+app.use('/api',testRoutes)
+
+
+const port = process.env.PORT || 8080;
+
+
+mongoose
+  .connect(process.env.DATABASE,{ useNewUrlParser: true ,useUnifiedTopology: true}
+  )
+  .then(result => {
+    app.listen(port);
+  })
+  .catch(err => console.log(err));
+
+ 
